9c9b55da26cad146ea0ab64084e1355a9bac619f
ramyaparimi <rlp.parimi@gmail.com>
2021-04-22 06:37:12 -0500

    child to parent props

--- src/components/Expenses/NewExpense/ExpenseForm.js
+++ src/components/Expenses/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import './ExpenseForm.css'
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] =  useState('');
@@ -31,10 +31,12 @@ const ExpenseForm = () => {
             date: new Date(enteredDate)
         };
         
+        props.onSaveExpenseData(expenseData)
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
     }
+
 
     
     
