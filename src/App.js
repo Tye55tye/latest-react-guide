77e360daf587497a57e541f198ab3daffd2ea658
ramyaparimi <rlp.parimi@gmail.com>
2021-04-27 07:33:38 -0500

    child-parent, conditional lists

--- src/App.js
+++ src/App.js
@@ -1,48 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/Expenses/NewExpense/NewExpense';
 
+const DUMMy_EXPENSES = [
+  {
+    id: 'jdj120',
+    title: 'Car Insurance',
+    amount: 300,
+    date: new Date(2021, 4, 15)
+  },
+  {
+    id: 'jdj121',
+    title: 'Toilet Paper',
+    amount: 14.44,
+    date: new Date(2021, 5, 16)
+  },
+  {
+    id: 'jdj122',
+    title: 'New Desk',
+    amount: 400,
+    date: new Date(2020, 1, 19)
+  },
+  {
+    id: 'jdj123',
+    title: 'New TV',
+    amount: 600,
+    date: new Date(2019, 11, 25)
+  },
+]
+
 const App = () => {
 
-  const expenses = [
-    {
-      id: 'jdj120',
-      title: 'Car Insurance',
-      amount: 300,
-      date: new Date(2021, 4, 15)
-    },
-    {
-      id: 'jdj121',
-      title: 'Toilet Paper',
-      amount: 14.44,
-      date: new Date(2021, 5, 16)
-    },
-    {
-      id: 'jdj122',
-      title: 'New Desk',
-      amount: 400,
-      date: new Date(2020, 1, 19)
-    },
-    {
-      id: 'jdj123',
-      title: 'New TV',
-      amount: 600,
-      date: new Date(2019, 11, 25)
-    },
-  ]
+  const [expenses, setExpenses] = useState(DUMMy_EXPENSES);
 
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
-  }
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
 
+
+
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses}/>
-
+      <Expenses items={expenses} />
     </div>
   );
 }
